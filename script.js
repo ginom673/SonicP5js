60008921cad82b7da8d38b13555d6e83760a69ba
Glitch (hello-webpage) <none>
2019-12-18 00:56:52 +0000

    🎰🛰 Checkpoint ./script.js:5959929/399

--- script.js
+++ script.js
@@ -3,19 +3,30 @@ function setup()
 {
   createCanvas(1850,1000);
   background(0);
+  textStyle(BOLD);
+  textSize(32);
 }
 
 // constantly called to update the screen 
 function draw()
 {
+  
+  // draws P1's 
   fill(255, 90, 90);
   rect(0, 600, 500, 400);
+  
   fill(90, 90, 255);
   rect(1350, 600, 500, 400);
-  fill(200, 0, 0);
-  textSize(32);
-  textStyle(BOLD);
+  
+  
+  fill(200, 0, 0);  
   text('Text Here', 20, 640);
+  
   fill(0, 0, 200);
   text('Text There', 1370, 640);
 }
+var startY = 650;
+var endY = 900;
+var numCommands = 5;
+var textBoxHeight = endY - startY;
+var textInterval = textBoxHeight / (numCommands - 1);
