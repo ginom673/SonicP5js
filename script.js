0eb8875cc128061ca7bab606eaa5b5b686e8c74e
Glitch (hello-webpage) <none>
2019-12-18 00:08:41 +0000

    💰👠 Checkpoint ./index.html:5959929/3299 ./script.js:5959929/435

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// called at the beginning
+function setup()
+{
+  background(0);
+  fullscreen();
+}
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+// constantly called to update the screen 
+function draw()
+{
+  
+}
