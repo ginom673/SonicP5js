2aa04159bc04756115cbdaee6737d2dc10c34535
Glitch (hello-webpage) <none>
2019-12-18 00:22:50 +0000

    📻🚟 Checkpoint ./script.js:5959929/208

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // called at the beginning
 function setup()
 {
+  createCanvas(1850,1000);
   background(0);
-  fullscreen();
 }
 
 // constantly called to update the screen 
 function draw()
 {
-  
+  fill('');
+  rect(0, 600,500,400);
 }
