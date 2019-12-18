972fa1c0de1cd51840e53e2489cf2d032931bd7e
Glitch (hello-webpage) <none>
2019-12-18 00:33:55 +0000

    🛀🍦 Checkpoint ./script.js:5959929/330

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ function setup()
 // constantly called to update the screen 
 function draw()
 {
-  fill('');
-  rect(0, 600,500,400);
+  fill(255, 90, 90);
+  rect(0, 600, 500, 400);
+  fill(90, 90, 255);
+  rect(1350, 600, 500, 400);
+  fill(200, 0, 0);
+  textSize(32);
+  textStyle(BOLD);
+  text('text', 20, 640);
 }
