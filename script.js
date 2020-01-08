a7b31dca906f06efbc4f254e63a22d2ea902f7fd
Glitch (hello-webpage) <none>
2020-01-08 00:03:43 +0000

    🌌🚲 Checkpoint ./.glitch-assets:5959929/436 ./script.js:5959929/544

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 // make the player choices invisible on the screen... aka rather than moving up and down with some selector, they just press 6 to do command 6
 
 
+var fighterImg;
+
 // called at the beginning
 function setup()
 {
@@ -11,6 +13,7 @@ function setup()
   background(0);
   textStyle(BOLD);
   textSize(32);
+  fighterImg = loadImage("https://cdn.glitch.com/3c8bb0ef-34b4-4b1b-8044-7b2c1b6c0326%2Foldschool%20sonic%20removebg.png?v=1578441223317");
 }
 
 // constantly called to update the screen 
@@ -28,6 +31,10 @@ function draw()
   rect(800, 200, 250, 20);
   fill("blue");
   rect(1025, 300, 250, 20);
+  
+  // draw fighter
+  // GINO - test platform locations here!
+  image(fighterImg, p1x, p1y, 66, 82);
   
   // draws P1's UI
   fill(255, 90, 90);
@@ -60,3 +67,11 @@ var endY = 950;
 var numCommands = 6;
 var textBoxHeight = endY - startY;
 var textInterval = textBoxHeight / (numCommands - 1);
+
+
+// coordinates for the centers of the platforms
+
+var p1x = 650;
+var p1y = 220;
+//var p2x = 
+
