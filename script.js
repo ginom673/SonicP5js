4e9bacfad6ba8d87387a40bc60673f5ef2b29b63
Glitch (turnbased-fighter) <none>
2020-08-29 20:00:12 +0000

    ⛈🏡 Checkpoint ./.glitch-assets:5959929/378 ./script.js:5959929/362

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // GLOBAL VARIABLES
 
+var sonicImg;
 
 function setup()
 {
+  createCanvas(800,600);  
+  sonicImg = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fsonicdashrun.gif?v=1598730627392");
+  sonicImg.position(100, 100);
   
 }
 
 function draw()
 {
-  
+  image(sonicImg, 50, 50);
 }
