7d4b70e760d882f487904a15e6ecfe2b3287d4ba
Glitch (turnbased-fighter) <none>
2020-09-05 18:18:47 +0000

    🏟🕍 Checkpoint ./script.js:5959929/205:19534746/376 ./style.css:5959929/2

--- script.js
+++ script.js
@@ -2,19 +2,35 @@
 
 var sonicImgIdle;
 
+class Player
+{
+  
+  // give the Player class the following properties:
+  // x
+  // y
+  // vx
+  // vy
+  // onGround
+  // imageName
+  
+  
+  constructor(x, y, vx, vy, onGround, imgName)
+  {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.onGround = onGround;
+    this.imgName = imgName;
+  }
+}
+
 function setup()
 {
-  createCanvas(1280,721);  
+  createCanvas(1152,649);  
   sonicImgIdle = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2FSonic_Idle.png?v=1599326602917");
   sonicImgIdle.position(100, 100);
   
-  class Player
-    {
-      constructor(placeholder)
-      {
-        
-      }
-    }
 }
 
 function draw()
