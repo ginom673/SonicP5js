314c3119efa67269a2b8ef3cb56d735e36d29d4b
Glitch (turnbased-fighter) <none>
2020-09-05 18:52:58 +0000

    ☎️⛪️ Checkpoint ./script.js:5959929/156:19534746/780 ./style.css:19534746/22

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 var sonicImgIdle;
 
+var groundY = 576;
+var gravity = -0.2;
+
 class Player
 {
   
@@ -23,14 +26,33 @@ class Player
     this.onGround = onGround;
     this.imgName = imgName;
   }
+  
+  jump()
+  {
+    this.onGround = false;
+    this.vy = 5;
+  }
 }
+
+// create a variable groundY and set it to sonic's starting position
+// create a variable gravity and set it to -0.2
+// create a function called jump()
+// within jump, set sonic's onGround property to false
+// and also set sonic's vy to 5
+// then within our draw function, add code which:
+// 1 - increases sonic's y by vy
+// 2 - decreases sonic's vy by gravity
+// 3 - checks if sonic's y is <= groundY. if so, set vy to 0.
+// lastly, add code in our jump() function that checks if sonic is NOT onGround
+// if so, return (jump does nothing if we are already in the air)
 
 function setup()
 {
-  createCanvas(1280,721);  
+  //createCanvas(1280,721);  
+  createCanvas(1152,649);  
+  background("#fffff0");
   sonicImgIdle = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2FSonic_Idle.png?v=1599326602917");
-  sonicImgIdle.position(100, 100);
-  
+  sonicImgIdle.position(100, 500);  
 }
 
 function draw()
