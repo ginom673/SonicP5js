bae6ffb489a1b6729ec068918fe5c97f5bef5137
Glitch (turnbased-fighter) <none>
2020-09-05 17:54:47 +0000

    🌎🏞 Checkpoint ./script.js:5959929/159

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function setup()
   sonicImgIdle = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2FSonic_Idle.png?v=1599326602917");
   sonicImgIdle.position(100, 100);
   
+  class Player
+    {
+      constructor(placeholder)
+      {
+        
+      }
+    }
 }
 
 function draw()
