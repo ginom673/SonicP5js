a6d9a61a97dde43aa193355d175180c02a563f3b
Glitch (turnbased-fighter) <none>
2020-10-24 17:48:45 +0000

    🌲⏱ Checkpoint ./script.js:5959929/735:19534746/1072

--- script.js
+++ script.js
@@ -165,11 +165,12 @@ function setup()
 {
   //createCanvas(1280,721);  
   createCanvas(1152,649);  
+  
   background("#fffff0");
   
   background1 = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_background_loop.gif?v=1601145979742");
   background1.position(38, 38);
-  background1.resize(1200, 776);
+  background1.size(1200, 776);
   //background1.remove();
   //background1 = loadImage("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_background_loop.gif?v=1601145979742");
   
