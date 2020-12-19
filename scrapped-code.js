dd46ee55f77c24b51f002cc9433e66e9b1c4f36e
Glitch (turnbased-fighter) <none>
2020-12-19 19:23:37 +0000

    🏓🍏 Checkpoint ./script.js:5959929/3535:19534746/128 ./scrapped-code.js:5959929/2695

---
+++ scrapped-code.js
@@ -0,0 +1,60 @@
+//background1 = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_background_loop.gif?v=1601145979742");
+//background1.position(38, 38);
+//background1.size(1200, 776);
+  
+//background1.remove();
+//background1 = loadImage("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_background_loop.gif?v=1601145979742");
+
+//fill("#32a83a");
+
+// platform1.addTile(someTile)
+  
+//fill("#b56d1b");
+// platform2 = new Platform(0, 620, 1150, 25, "#b56d1b");
+
+/*
+var tile1 = new Tile(0, 524, "#FF0000");
+var tile2 = new Tile(tileDefaultW, 524, "#FF0000");
+var tile3 = new Tile(tileDefaultW * 2, 524, "#FF0000");
+var tile4 = new Tile(tileDefaultW * 3, 524, "#FF0000");
+var tile5 = new Tile(tileDefaultW * 4, 524, "#FF0000");
+var tile6 = new Tile(tileDefaultW * 5, 524, "#FF0000");
+var tile7 = new Tile(tileDefaultW * 6, 524, "#FF0000");
+var tile8 = new Tile(tileDefaultW * 7, 524, "#FF0000");
+var tile9 = new Tile(tileDefaultW * 8, 524, "#FF0000");
+var tile10 = new Tile(tileDefaultW * 9, 524, "#FF0000");
+var tile11 = new Tile(tileDefaultW * 10, 524, "#FF0000");
+var tile12 = new Tile(tileDefaultW * 11, 524, "#FF0000");
+var tile13 = new Tile(tileDefaultW * 12, 524, "#FF0000");
+var tile14 = new Tile(tileDefaultW * 13, 524, "#FF0000");
+var tile15 = new Tile(tileDefaultW * 14, 524, "#FF0000");
+var tile16 = new Tile(tileDefaultW * 15, 524, "#FF0000");
+var tile17 = new Tile(tileDefaultW * 16, 524, "#FF0000");
+var tile18 = new Tile(tileDefaultW * 17, 524, "#FF0000");
+var tile19 = new Tile(tileDefaultW * 18, 524, "#FF0000");
+var tile20 = new Tile(tileDefaultW * 2, 524, "#FF0000");
+
+platform1.addTile(tile1);
+platform1.addTile(tile2);
+platform1.addTile(tile3);
+platform1.addTile(tile4);
+platform1.addTile(tile5);
+platform1.addTile(tile6);
+platform1.addTile(tile7);
+platform1.addTile(tile8);
+platform1.addTile(tile9);
+platform1.addTile(tile10);
+platform1.addTile(tile11);
+platform1.addTile(tile12);
+platform1.addTile(tile13);
+platform1.addTile(tile14);
+platform1.addTile(tile15);
+platform1.addTile(tile16);
+platform1.addTile(tile17);
+platform1.addTile(tile18);
+// platform1.addTile(tile19);
+*/
+//rect(50, 50, screenWidth - 50, screenHeight - 50);
+//rect(borderWidth/2 - 50, borderWidth/2 + 50, screenWidth - borderWidth, screenHeight - borderWidth)
+//rect(borderWidth/2 + 50,borderWidth/2 - 50,1280-borderWidth+20,721-borderWidth);
+//rect(0,0,1152,649);
--- script.js
+++ script.js
@@ -39,8 +39,7 @@ class Player
   // jump
   jump()
   {
-    // prevent double-jumping
-    // if player tries to jump while in midair, do nothing
+    // prevent double-jumping; if player tries to jump while in midair, do nothing
     if (this.onGround == false) 
     {
       return;
@@ -57,10 +56,8 @@ class Player
     for (var i=0; i < platform1.tiles.length; i++)
     {
       var collisionStatus = collide(sonic, platform1.tiles[i]);
-      // console.log(collisionStatus);
       if (collisionStatus != 'none')
       {
-        // console.log(collisionStatus);
         // sloppy workaround - set sonic's y based on the tile's y position that we collided with
         this.y = platform1.tiles[i].y - this.h;
         this.onGround = true;
@@ -110,10 +107,8 @@ class Platform
       // dont worry about its x y properties and stuff yet ... ill help there in a minute
       // then add the newly created tile to tiles
       var theTile = new Tile(x + (i * tileDefaultW), y, tileImgName);
-      this.tiles.push(theTile);
-      
-    }
-    
+      this.tiles.push(theTile);      
+    }    
   }
   
   // display this Platform by displaying each of tiles  
@@ -173,21 +168,7 @@ class Tile
   }
 }
 
-// create a variable groundY and set it to sonic's starting position
-// create a variable gravity and set it to -0.2
-// create a function called jump()
-// within jump, set sonic's onGround property to false
-// and also set sonic's vy to 5
-// then within our draw function, add code which:
-// 1 - increases sonic's y by vy
-// 2 - decreases sonic's vy by gravity
-// 3 - checks if sonic's y is <= groundY. if so, set vy to 0.
-// lastly, add code in our jump() function that checks if sonic is NOT onGround
-// if so, return (jump does nothing if we are already in the air)
-
 var sonic;
-
-
 
 // taken from https://stackoverflow.com/questions/29861096/detect-which-side-of-a-rectangle-is-colliding-with-another-rectangle
 function collide(r1,r2)
@@ -214,28 +195,9 @@ var tile1;
 function setup()
 {
   //createCanvas(1280,721);  
-  createCanvas(screenWidth,screenHeight);  
-  
-  //background1 = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_background_loop.gif?v=1601145979742");
-  //background1.position(38, 38);
-  //background1.size(1200, 776);
-  
-  //background1.remove();
-  //background1 = loadImage("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_background_loop.gif?v=1601145979742");
-  
-  
-  //fill("#32a83a");
-  // create a platform object using these values
-  
-  // let's create a platform object where
-  // the values you gave here for a rectangle
-  // will actually be given directly to the Platform constructor
+  createCanvas(screenWidth,screenHeight);
   platform1 = new Platform(0, groundY, 20, 1, "https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2Fgreen_hill_ground_flat.png?v=1601140825013");
   
-  // platform1.addTile(someTile)
-  
-  //fill("#b56d1b");
-  // platform2 = new Platform(0, 620, 1150, 25, "#b56d1b");
   
   // sonicImgNormal = loadImage("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2FSonic_Run.gif?v=1599326604172");
   sonicImgNormal = createImg("https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2FSonic_Run.gif?v=1599326604172");
@@ -244,48 +206,6 @@ function setup()
   
   sonic = new Player(100, 200, 0, 0, false, "sprite", 64, 72);
   
-  /*
-  var tile1 = new Tile(0, 524, "#FF0000");
-  var tile2 = new Tile(tileDefaultW, 524, "#FF0000");
-  var tile3 = new Tile(tileDefaultW * 2, 524, "#FF0000");
-  var tile4 = new Tile(tileDefaultW * 3, 524, "#FF0000");
-  var tile5 = new Tile(tileDefaultW * 4, 524, "#FF0000");
-  var tile6 = new Tile(tileDefaultW * 5, 524, "#FF0000");
-  var tile7 = new Tile(tileDefaultW * 6, 524, "#FF0000");
-  var tile8 = new Tile(tileDefaultW * 7, 524, "#FF0000");
-  var tile9 = new Tile(tileDefaultW * 8, 524, "#FF0000");
-  var tile10 = new Tile(tileDefaultW * 9, 524, "#FF0000");
-  var tile11 = new Tile(tileDefaultW * 10, 524, "#FF0000");
-  var tile12 = new Tile(tileDefaultW * 11, 524, "#FF0000");
-  var tile13 = new Tile(tileDefaultW * 12, 524, "#FF0000");
-  var tile14 = new Tile(tileDefaultW * 13, 524, "#FF0000");
-  var tile15 = new Tile(tileDefaultW * 14, 524, "#FF0000");
-  var tile16 = new Tile(tileDefaultW * 15, 524, "#FF0000");
-  var tile17 = new Tile(tileDefaultW * 16, 524, "#FF0000");
-  var tile18 = new Tile(tileDefaultW * 17, 524, "#FF0000");
-  var tile19 = new Tile(tileDefaultW * 18, 524, "#FF0000");
-  var tile20 = new Tile(tileDefaultW * 2, 524, "#FF0000");
-  
-  platform1.addTile(tile1);
-  platform1.addTile(tile2);
-  platform1.addTile(tile3);
-  platform1.addTile(tile4);
-  platform1.addTile(tile5);
-  platform1.addTile(tile6);
-  platform1.addTile(tile7);
-  platform1.addTile(tile8);
-  platform1.addTile(tile9);
-  platform1.addTile(tile10);
-  platform1.addTile(tile11);
-  platform1.addTile(tile12);
-  platform1.addTile(tile13);
-  platform1.addTile(tile14);
-  platform1.addTile(tile15);
-  platform1.addTile(tile16);
-  platform1.addTile(tile17);
-  platform1.addTile(tile18);
-  //  platform1.addTile(tile19);
-  */
   
 }
 
@@ -298,18 +218,9 @@ function draw()
   
   background("#82ebed");
   
-  // draw background
-  //image(background1, 0, 0);
-  
   var borderWidth = 6;
   stroke(borderWidth);
   noFill();
-  //rect(50, 50, screenWidth - 50, screenHeight - 50);
-  //rect(borderWidth/2 - 50, borderWidth/2 + 50, screenWidth - borderWidth, screenHeight - borderWidth)
-  //rect(borderWidth/2 + 50,borderWidth/2 - 50,1280-borderWidth+20,721-borderWidth);
-  //rect(0,0,1152,649);
-  
-  // console.log(platform1.tiles[0].h);
   
   // update sonic position and speed
   if(!sonic.onGround)
@@ -333,14 +244,8 @@ function draw()
   platform1.display();
   //platform2.display();
   
-  // draw tile1
-  // console.log(tile1);
-  // console.log(sonic.w);
-  // console.log(sonic.h);
-  // tile1.draw();
-  // alert("pause");
   
-  
+
   
   // draw sonic image
   sonicImgNormal.position(sonic.x, sonic.y);  
