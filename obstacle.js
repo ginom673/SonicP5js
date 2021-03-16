d481e4513c25d4bb822c15d909f8cc242e417035
Glitch (sonic-platformer) <none>
2021-03-16 21:26:47 +0000

    🌾❣️ Checkpoint ./obstacle.js:5959929/16:19534746/1183 ./script.js:19534746/210

--- obstacle.js
+++ obstacle.js
@@ -1,74 +1,24 @@
 // Obstacle is any game object besides a platform that does not move (inanimate objects)
 // unlike platforms, you do not land on obstacles
 class Obstacle
-{
+{  
   
-  /*
-  
-  constructor(x, y, vx, vy, onGround, imgName, w, h, hx, hy, hw, hh, isGif)
+  constructor(x, y, w, h, imgName, hx, hy, hw, hh, isGif)
   {
     this.x = x;
     this.y = y;
-    this.vx = vx;
-    this.vy = vy;
-    this.onGround = onGround;
+    this.w = w;
+    this.h = h;
+    this.hx = hx;
+    this.hy = hy;
+    this.hw = hw;
+    this.hh = hh;
     this.isGif = isGif;
-    this.hx = hx;
-    this.hy = hy;
-    this.hw = hw;
-    this.hh = hh;
-    this.imgName = imgName;
-    this.img = createImg(imgName);
-    this.img.position(x,y);
-    this.w = w;
-    this.h = h;
-    
-    // whether or not character is considered living
-    this.isAlive = true;
-    
-    // whether or not this character uses collisions
-    this.hitboxActive = true;
-    
-    // whether or not this character's image should be displayed
-    // (the Character object could be hidden but still interactable)
-    this.visible = true;
-  }
-  
-  // used by draw() to display character
-  display()
-  {
-
-    if (!this.visible)
-    {         
-      return;
-    }
-
-    if(this.isGif)
-    {
-      this.img.position(this.x, this.y);      
-    }
-    else
-    {
-      image(this.img, this.x, this.y);
-    }  
-
-  }
-  
-  */
-  
-  
-  constructor(x, y, w, h, imgName, hx, hy, hw, hh)
-  {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.hx = hx;
-    this.hy = hy;
-    this.hw = hw;
-    this.hh = hh;
     this.imgName = imgName;    
-    this.image = loadImage(imgName);
+    this.image = createImg(imgName);
+    this.image.position(x,y);
+    this.visible = true;
+    this.hitboxActive = true;
   }
   
   display()
@@ -80,11 +30,11 @@ class Obstacle
 
     if(this.isGif)
     {
-      this.img.position(this.x, this.y);      
+      this.image.position(this.x, this.y);
     }
     else
     {
-      image(this.img, this.x, this.y);
+      image(this.image, this.x, this.y);
     }
   }
   
