c19a5680aa2db8492c0669d75e74bb6083e9db05
Glitch (sonic-platformer) <none>
2021-08-21 18:22:30 +0000

    🎈💎 Checkpoint ./tile.js:5959929/20:19534746/766

--- tile.js
+++ tile.js
@@ -36,14 +36,26 @@ class Tile
     }
     if(imgName == "https://cdn.glitch.com/6e344420-4b09-4670-a529-dc21e1a4da32%2FGreen_Hill_Slope_Down.png?v=1615096004205")
     { 
+      
+      // NOTE: some of these points are shared between lines, BUT, if you use the same point object for different lines,
+      // then autoscrolling will cause the shared points to be autoscrolled twice as often as they should be
+      // to fix this, we can simply COPY the points
+      
       var line1start = new Point(this.x + tileHorizontalOffset + 25, this.y + tileVerticalOffset + 255);
-      var line2start = new Point(this.x + this.w / 2 + tileHorizontalOffset, this.y + tileVerticalOffset + 300);
-      var line3start = new Point(this.x + this.w + tileHorizontalOffset, this.y + tileVerticalOffset - 2);
-      var line4start = new Point(this.x + this.w + tileHorizontalOffset, this.y + tileVerticalOffset - 25);
-      var line4end = new Point(this.x + this.w + tileHorizontalOffset, this.y + tileVerticalOffset - 2);      
-      var line = new Line2D(line1start, line2start);
-      var line2 = new Line2D(line2start, line3start);
-      var line3 = new Line2D(line3start, line4start);
+      var line1end = new Point(this.x + this.w / 2 + tileHorizontalOffset, this.y + tileVerticalOffset + 300);
+      
+      var line2start = JSON.parse(JSON.stringify(line1end));
+      var line2end = new Point(this.x + this.w + tileHorizontalOffset, this.y + tileVerticalOffset - 2);
+      
+      var line3start = JSON.parse(JSON.stringify(line2end));
+      var line3end = new Point(this.x + this.w + tileHorizontalOffset, this.y + tileVerticalOffset - 25);
+      
+      var line4start = JSON.parse(JSON.stringify(line3end));
+      var line4end = new Point(this.x + this.w + tileHorizontalOffset, this.y + tileVerticalOffset - 2);     
+      
+      var line = new Line2D(line1start, line1end);
+      var line2 = new Line2D(line2start, line2end);
+      var line3 = new Line2D(line3start, line3end);
       var line4 = new Line2D(line4start, line4end);
       this.slopes.push(line);
       this.slopes.push(line2);
