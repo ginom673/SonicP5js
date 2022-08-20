e4a6dd1e7a7d3ea7950813aab571e89414c5d617
Glitch (sonic-platformer) <none>
2022-08-20 17:44:59 +0000

    🏵🌗 Checkpoint ./hbox-drawing.js:5959929/201:19534746/936

--- hbox-drawing.js
+++ hbox-drawing.js
@@ -113,15 +113,29 @@ function nextDrawing()
   
 }
 
+var currentTileHitboxes = [];
+
 // writeLine is some JANKY code
-// the goal of this function is to write 
+// the goal of this function is to console.log() some text which can be copy-pasted back into this file as legitimate code
+// this is done to avoid the problem of writing out persistent data using JSONs, Node.js, etc.
+// so instead of having the code automatically write out these points to some data table,
+// we have it write out lines of code which, after being copy-pasted, could effectively serve the purpose, and persist in the code
+// this function in particular writes out the line of code that would let us add a new 2D line to a list of lines
+// that will later be loaded into the tileData dictionary
 function writeLine(newLine)
 {
-  console.log("new Line2D(new Point(" + newLine.p1.x + ", " + newLine.p1.y + "), new Point(" + newLine.p2.x + ", " + newLine.p2.y + "))");
+  
+  // step 2 - modify this console.log() string code so that the new Line2D() created here will be added to the currentTileHitboxes list
+  var writtenLine = "new Line2D(new Point(" + newLine.p1.x + ", " + newLine.p1.y + "), new Point(" + newLine.p2.x + ", " + newLine.p2.y + "))";
+  console.log(writtenLine);
+  currentTileHitboxes.push(writtenLine);
 }
 
-// 
+// initTileHitboxes will initialize the hitboxes of all the tiles
+// this is where are the copy-pasted lines from writeLine will go!
 function initTileHitboxes()
 {
+  
+  // step 1 - create a blank list here called currentTileHitboxes
   
 }
